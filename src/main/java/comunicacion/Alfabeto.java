01efa9e659d1ac7e65ac469233525ad415bec287
cquinchiar <cquinchiar@unal.edu.co>
2020-10-10 15:35:00 -0500

    primer error

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,45 @@
+package comunicacion;
+
+public class Alfabeto extends Pictograma{
+	private String[] letras;
+	private String interpretacion;
+	
+	public Alfabeto (String origen, String[] letras, String interpretacion) {
+		super(origen);
+		this.letras=letras;
+		this.interpretacion=interpretacion;
+	}
+
+	public String[] getLetras() {
+		return letras;
+	}
+
+	public void setLetras(String[] letras) {
+		this.letras = letras;
+	}
+
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	public int cantidadLetras() {
+		return this.getLetras().length;
+	}
+	public String interpretacion() {
+		return this.getInterpretacion();
+	}
+	public String toString() {
+		String palabra="";
+		for (int i=0;i<this.getLetras().length;i++) {
+			if (i==this.getLetras().length-1) {
+				palabra+=this.letras[i];
+			}else {
+				palabra+=this.letras[i]+", ";
+			}
+		}return palabra;
+	}
+
+}
